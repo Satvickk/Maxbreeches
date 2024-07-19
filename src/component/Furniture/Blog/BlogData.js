4d742efb631c2ea8995b5c950ad91676382a3d95
Satvickk <lenovo37819@gmail.com>
2024-07-19 12:17:51 +0530

    Updated changes

--- src/component/Furniture/Blog/BlogData.js
+++ src/component/Furniture/Blog/BlogData.js
@@ -1,7 +1,5 @@
 import Blog1 from '../../../assets/img/product-image/11.png'
-import Blog2 from '../../../assets/img/product-image/15.png'
-
- 
+import Blog2 from '../../../assets/img/product-image/sg-logo.jpg'
 
 export const BlogData = [
     {   
@@ -9,6 +7,14 @@ export const BlogData = [
         date: `11 May 2024`,
         heading: "Enhancing Quality Through Advanced Technologies and Systems",
         para: `In today's competitive market, maintaining high-quality standards is essential for any business. At our company, we prioritize quality at every step, ensuring that our products meet the highest standards. In this blog post, we will explore how we leverage advanced technologies and specialized systems to enhance our quality control processes.`,
+        button: "Read More..."
+       
+    },
+    {   
+        img: Blog2,
+        date: `3-5 February 2023`,
+        heading: "Spoga gafa Cologne",
+        para: `You are most welcome at our stand in O-68 Hall 7.1 for the forthcoming Spoga horse on 3-5 February 2023 Cologne Germany.`,
         button: "Read More..."
        
     }
