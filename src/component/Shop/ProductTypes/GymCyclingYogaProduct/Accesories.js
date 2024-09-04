205d2483110a541edf8496c6420ccb9d89e66920
Satvickk <lenovo37819@gmail.com>
2024-09-04 15:34:29 +0530

    fixed gym,cycling,yoga products

--- src/component/Shop/ProductTypes/GymCyclingYogaProduct/Accesories.js
+++ src/component/Shop/ProductTypes/GymCyclingYogaProduct/Accesories.js
@@ -1,44 +1,13 @@
 import React from "react";
-import ListCard from "../../ListCard";
-import data from "../../ProductsData.json"; // Assuming your JSON data is in a file named 'ProductsData.json'
-import Banner from "../../../Common/Banner";
+import SubTypeBanner from "../SubProductType/SubTypeBanner"
 
-const Accesories = () => {
+const LadiesWear = () => {
+
   return (
     <>
-    <Banner title="Accessories" />
-      <section id="shop_main_area" className="ptb-100">
-        <div className="container">
-          <div className="row align-items-center justify-content-center">
-            <div className="col-lg-6 text-center">
-              <ul className="pagination d-flex justify-content-between align-items-center">
-                <li className="page-item">
-                  <span className="page-link" aria-label="Previous"></span>
-                </li>
-                <li>
-                  <div className="product_filter">
-                    <div className="product_shot_title">
-                      <p className="text-2xl mb-0">Accessories</p>
-                    </div>
-                  </div>
-                </li>
-                <li className="page-item">
-                  <span className="page-link" aria-label="Previous"></span>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="row">
-            {data.filter((item) => item.collectionName === "Accessories").map((item) => (
-              item?.children.map((product, index) => (
-                <ListCard data={product} key={index} />
-              ))
-            ))}
-          </div>
-        </div>
-      </section>
+    <SubTypeBanner heading={"Accessories"} subCollectionName={"Accessories"} title={"Accessories"} slug={'Accessories'}/>
     </>
   );
 };
 
-export default Accesories;
+export default LadiesWear;
---
+++ src/component/Shop/ProductsData.js
@@ -0,0 +1,203 @@
+import img1 from "../../assets/img/product-image/update-img/breeches.jpg"
+import img6 from "../../assets/img/product-image/update-img/m.breeches.jpg";
+import img10 from "../../assets/img/product-image/update-img/kids base.jpg";
+
+
+
+
+
+// import lb1 from "/lb1.jpg"
+
+export const SubProductType = [
+  {
+    id: 1,
+    subCollectionName: "Ladies Wear",
+    children: [
+      {
+        img: img1,
+        title: "Ladies Wear",
+        slug: "LadiesWear",
+        products: [
+          {
+            productId: 1,
+            productName: "LRW-Breeches--001",
+            productDescription:
+              "Ovation Horse Riding Pants Breeches with Knee-Patch and Zipper Pocket for Women.The breeches hug you all around and give you the style and confidence that you so desire when riding. These Women's Horse Riding tights are multi-functional, for any type of sport",
+            Image: ["lb1.jpg", "lb2.jpg","lb3.jpg", "lb4.jpg"],
+            price: 20,
+            rating: 4.5,
+            reviewCount: 30,
+          },
+          {
+            productId: 2,
+            productName: "LRW-Breeches--002 ",
+            productDescription:
+              "Very classic Real denim made of cotton with wash effect with leather full seat for strong And long lasting breeches.Pull Up Comfort: Wide waistband with belt loops delivers a comfortable fit and stay-put confidence",
+            Image:["lb11.jpg", "lb12.jpg","lb13.jpg", "lb14.jpg", "lb15.jpg","lb16.jpg", "lb17.jpg"],
+            price: 40,
+            rating: 4.0,
+            reviewCount: 25,
+          },
+          {
+            productId: 3,
+            productName: " LRW-Breeches-003",
+            productDescription:
+              "Women's fashion breech two tone made of blend of cotton and nylon 4way stretch With bottom flexi panel to ride with passion and energy.",
+              Image: ["lb21.jpg", "lb22.jpg","lb23.jpg"],
+            price: 60,
+            rating: 4.8,
+            reviewCount: 40,
+          },
+          {
+            productId: 4,
+            productName: "LRW-Breeches-004 ",
+            productDescription:
+              "women's long lasting combination of fashion and classic made of blend of polyester and viscose with branding details, two front slant pocket with back two flap pocket, silicon knee patch with lycra bottom.",
+              Image: ["lb31.jpg", "lb32.jpg","lb33.jpg", "lb34.jpg", "lb35.jpg", "lb36.jpg"],
+            price: 50,
+            rating: 4.2,
+            reviewCount: 35,
+          },
+          {
+            productId: 5,
+            productName: "LRW-Breeches-005",
+            productDescription:
+              "Ovation Horse Riding Pants Breeches with Knee-Patch and Zipper Pocket for Women.The ladies riding tights breeches are smooth and sleek, offer a fantastic close contact fit shaping well to your body, perfect for all leisure activities as well as showing or schooling.",
+              Image: ["lb41.jpg", "lb42.jpg","lb43.jpg", "lb44.jpg"],
+            price: 50,
+            rating: 4.2,
+            reviewCount: 35,
+          },
+          {
+            productId: 6,
+            productName: "LRW-Breeches-006",
+            productDescription:
+              "Brand FitsT4 Women's Riding Tights Knee Patch Ventilated Active Equestrian Schooling Tights",
+              Image: ["lb51.jpg", "lb52.jpg","lb53.jpg"],
+            price: 50,
+            rating: 4.2,
+            reviewCount: 35,
+          },
+          {
+            productId:7,
+            productName: "LRW-Breeches-007",
+            productDescription:
+              "Willit Women's Riding Tights Knee-Patch Breeches Equestrian Horse Riding Pants Schooling Tights Zipper Pockets ",
+              Image: ["lb61.jpg", "lb62.jpg","lb63.jpg", "lb64.jpg"],
+            price: 50,
+            rating: 4.2,
+            reviewCount: 35,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    subCollectionName: "MensWear",
+    children: [
+      {
+        img: img6,
+        title: "MensWear",
+        slug: "MensWear",
+        products: [
+          {
+            productId: 1,
+            productName: "MRW-Breeches-001",
+            productDescription:
+              "Ever best Men’s classic woven made of cotton microfiber with silicon full seat And bottom lycra for the comfort even under the shoes.",
+            Image: ["MB1.jpg","MB2.jpg","MB3.jpg"],
+            price: 20,
+            rating: 4.5,
+            reviewCount: 30,
+          },
+          {
+            productId: 2,
+            productName: "MRW-Breeches-002",
+            productDescription:
+              "Very classic Real denim made of cotton with wash effect with leather full seat for strong And long lasting breeches.",
+            Image: ["MB5.jpg","MB6.jpg","MB7.jpg"],
+            price: 40,
+            rating: 4.0,
+            reviewCount: 25,
+          },
+          {
+            productId: 3,
+            productName: "MRW-Breeches-003",
+            productDescription:
+              "Men’s fashion breech two tone made of blend of cotton and nylon 4way stretch With bottom flexi panel to ride with passion and energy.",
+            Image: ["MB14.jpg","MB15.jpg","MB20.jpg"],
+            price: 60,
+            rating: 4.8,
+            reviewCount: 40,
+          },
+          {
+            productId: 4,
+            productName: "MRW-Breeches-004",
+            productDescription:
+              "Men’s long lasting combination of fashion and classic made of blend of polyester and viscose with branding details, two front slant pocket with back two flap pocket, silicon knee patch with lycra bottom.",
+            Image: ["MB11.jpg","MB12.jpg","MB13.jpg"],
+            price: 50,
+            rating: 4.2,
+            reviewCount: 35,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    subCollectionName: "Accessories",
+    children: [
+      {
+        img: img10,
+        title: "Accessories",
+        slug: "Accessories",
+        products: [
+          {
+            productId: 1,
+            productName: "Men’s Classic Woven Breeches",
+            productDescription:
+              "Ever best Men’s classic woven made of cotton microfiber with silicon full seat And bottom lycra for the comfort even under the shoes.",
+            Image: "m1.jpg",
+            price: 20,
+            rating: 4.5,
+            reviewCount: 30,
+          },
+          {
+            productId: 2,
+            productName: "Real Denim Breeches",
+            productDescription:
+              "Very classic Real denim made of cotton with wash effect with leather full seat for strong And long lasting breeches.",
+            Image: "m2.jpg",
+            price: 40,
+            rating: 4.0,
+            reviewCount: 25,
+          },
+          {
+            productId: 3,
+            productName: "Men’s Fashion Breeches",
+            productDescription:
+              "Men’s fashion breech two tone made of blend of cotton and nylon 4way stretch With bottom flexi panel to ride with passion and energy.",
+            Image: "m3.jpg",
+            price: 60,
+            rating: 4.8,
+            reviewCount: 40,
+          },
+          {
+            productId: 4,
+            productName: "Men’s Long Lasting Breeches",
+            productDescription:
+              "Men’s long lasting combination of fashion and classic made of blend of polyester and viscose with branding details, two front slant pocket with back two flap pocket, silicon knee patch with lycra bottom.",
+            Image: "m4.jpg",
+            price: 50,
+            rating: 4.2,
+            reviewCount: 35,
+          },
+        ],
+      },
+    ],
+  },
+];
+
+
