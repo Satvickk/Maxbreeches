c643248c59c92a6d70e30db8941520e0fab92ac8
Satvickk <lenovo37819@gmail.com>
2024-09-12 18:26:44 +0530

    added space

--- src/component/Furniture/OurStory/index.js
+++ src/component/Furniture/OurStory/index.js
@@ -10,7 +10,7 @@ const OurStory = () => {
                 <div className="col-lg-8 offset-lg-2">
                     <div className="our_story_content">
                         <h2>Our Story</h2>
-                        <p><b>MaxBreeches</b>is an ISO 9001:2015 certified manufacturer, specializes in high-quality equestrian rider apparel. With skilled labor, advanced technology, and a dedicated quality team, we ensure top-notch products. We welcome small orders, value customer feedback, and guarantee timely delivery, supported by a robust ERP system.</p>
+                        <p><b>MaxBreeches </b>is an ISO 9001:2015 certified manufacturer, specializes in high-quality equestrian rider apparel. With skilled labor, advanced technology, and a dedicated quality team, we ensure top-notch products. We welcome small orders, value customer feedback, and guarantee timely delivery, supported by a robust ERP system.</p>
                         <Link to="/about" className="theme-btn-one bg-black btn_md">Read Full Story</Link>
                     </div>
                 </div>
